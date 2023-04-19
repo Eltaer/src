05ba6fdd3446d822872d86f64da924491daa36d4
Eltaer <95209215+Eltaer@users.noreply.github.com>
2023-04-19 09:20:59 +0300

    Создание класса Newsstand с массивом указателей

--- Magazine/Magazine/Program.cs
+++ Magazine/Magazine/Program.cs
@@ -10,27 +10,72 @@ namespace Magazine
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Введите размер массива: ");
         }
     }
 
     class Magazine
     {
-        private string Name;
 
-        private string Publisher;
+        public string Publisher;
 
-        private string Price;
+        public string ISSN_Num;
 
-        private string ISSN_Num;
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
 
-        
+        public int Price
+        {
+            get
+            {
+                return Price;
+            }
+            set
+            {
+                Price = value;
+            }
+        }
     }
 
     class Newsstand
     {
+        private Magazine[] mass;
 
+        Newsstand (int size)
+        {
+            mass = new Magazine [size];
+        }
+        
+        private void AddMagazine()
+        {
+            int size = mass.Length;
+            for (int i = 0; i < size; i++)
+            {
+                
+                Magazine magazine = new Magazine();
+                Console.Write("Название: ");
+                magazine.Name = Console.ReadLine();
 
+                Console.Write("Издатель: ");
+                magazine.Publisher = Console.ReadLine();
 
+                Console.Write("Цена: ");
+                magazine.Price = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Номер ISSN: ");
+                magazine.ISSN_Num = Console.ReadLine();
+
+            }
+
+        }
     }
 }
